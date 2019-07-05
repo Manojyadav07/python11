e0eda44dec43eef5d04bcc42ff8171ec62469471
Manojyadav07 <52398710+Manojyadav07@users.noreply.github.com>
2019-07-05 15:30:50 +0530

    Update manu13.py

--- manu13.py
+++ manu13.py
@@ -1,11 +1,13 @@
-l=int(input())
-temp=l
-rev=0
-while(l>0):
-    dig=l%10
-    rev=rev*10+dig
-    l=l//10
-if(temp==rev):
-    print("yes")
+n= int(input())
+ 
+temp = n
+rev = 0
+ 
+while temp != 0:
+	rev = (rev * 10) + (temp % 10)
+	temp = temp // 10
+ 
+if n == rev:
+	print("yes")
 else:
-    print("no")
+	print("no")
